0f91b07baf6ad4ba3c4b0cec783c043b17ac560a
Nour_Ashraf <nour.ashraf@webgenstudio.com>
2023-06-17 23:11:46 +0200

    Coming soon banner

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,27 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { CommonModule } from '@angular/common';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { SplashScreenComponent } from './shared/splash-screen/splash-screen.component';
 
+import { LottieModule } from 'ngx-lottie';
+import player from 'lottie-web';
+
+// Export this function
+export function playerFactory(): any {
+  return import('lottie-web');
+}
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
+  declarations: [AppComponent, SplashScreenComponent],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    CommonModule,
+    LottieModule.forRoot({ player: playerFactory }),
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
---
+++ src/app/shared/splash-screen/splash-screen.component.ts
@@ -0,0 +1,24 @@
+import { Component, OnInit } from '@angular/core';
+import { AnimationItem } from 'lottie-web';
+import { AnimationOptions } from 'ngx-lottie';
+
+@Component({
+  selector: 'app-splash-screen',
+  templateUrl: './splash-screen.component.html',
+  styleUrls: ['./splash-screen.component.scss'],
+})
+export class SplashScreenComponent implements OnInit {
+  // This is the option that uses the package's AnimationOption interface
+  options: AnimationOptions = {
+    path: '/assets/maintain.json',
+  };
+
+  constructor() {}
+
+  ngOnInit(): void {}
+
+  // This is the component function that binds to the animationCreated event from the package
+  onAnimate(animationItem: AnimationItem): void {
+    console.log(animationItem);
+  }
+}
